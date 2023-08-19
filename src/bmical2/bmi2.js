d970028088ac18e840d03d6605304e90b9c132ab
Gokulsv17 <gokulsvsv@gmail.com>
2023-08-20 00:16:45 +0530

    bg color

--- src/bmical2/bmi2.js
+++ src/bmical2/bmi2.js
@@ -5,7 +5,7 @@ const BMI2 = (props) => {
   const [height, setHeight] = useState(0);
   const [bmi, setBmi] = useState("");
   const [message, setMessage] = useState("");
-
+  const [background,setBackGround]=useState("white")
   let calBmi = (event) => {
     event.preventDefault();
     if (weight === 0 || height === 0) {
@@ -16,10 +16,13 @@ const BMI2 = (props) => {
 
       if (bmi < 25) {
         setMessage("you are underweight");
+        setBackGround("#edf606")
       } else if (bmi >= 25 && bmi < 30) {
         setMessage("you are healthy weight");
+        setBackGround("#1bf606")
       } else {
         setMessage("you are over weight");
+        setBackGround("#f6061f")
       }
     }
 
@@ -31,16 +34,16 @@ const BMI2 = (props) => {
   };
 
   return (
-    <div className="app">
-      <div className="container">
+    <div className="app" >
+      <div className="container" style={{backgroundColor:background}}>
         <h2 className="center">BMI Calculator</h2>
         <form onSubmit={calBmi}>
           <div>
-            <label>Weight</label>
+            <label>Weight (kg)</label>
             <input value={weight} onChange={(e) => setWeight(e.target.value)} />
           </div>
           <div>
-            <label>height</label>
+            <label>height (cm)</label>
             <input value={height} onChange={(e) => setHeight(e.target.value)} />
           </div>
           <div>
