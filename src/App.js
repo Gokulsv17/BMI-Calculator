c9db87b40a4875767b422da914a57dc5eb43ddd2
Gokulsv17 <gokulsvsv@gmail.com>
2023-08-23 23:19:13 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 // import BMICalculator from './bmi-calc/bmi_calculator';
-import Pie from "./pie/pie"
-import BMI2 from './bmical2/bmi2';
+import Pie from "./pie/pie";
+import BMI2 from "./bmical2/bmi2";
 function App() {
-  // const [bmiValue,setBmiValue]=useState()
-  // const [bmiContent,setBmiContent]= useState("")
-  // const [bmiDegValue,setBmiDegValue] =useState("45")
   return (
-   
     <div className="App">
-   {/* <BMICalculator bmiValue={setBmiValue} bmiCont={setBmiContent} bmiDeg={setBmiDegValue}/> */}
-   <BMI2 />
-   {/* <Pie bmiValue={bmiValue} bmiCont={bmiContent}/> */}
-   </div>
+      <BMI2 />
+    </div>
   );
 }
 
